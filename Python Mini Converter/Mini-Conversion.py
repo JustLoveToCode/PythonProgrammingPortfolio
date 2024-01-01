210cedc57ce223861803354b67bdff3b8be41de2
JustLoveToCode <135109684+JustLoveToCode@users.noreply.github.com>
2024-01-01 20:54:55 +0800

    Add files via upload
    This is my first commit

---
+++ Python Mini Converter/Mini-Conversion.py
@@ -0,0 +1,31 @@
+import PySimpleGUI as sg
+
+
+def km_to_miles(km):
+    return km / 1.6
+
+
+label = sg.Text("Kilometers: ")
+input_box = sg.InputText(tooltip="Enter todo", key="kms")
+miles_button = sg.Button("Convert")
+
+output = sg.Text(key="output")
+
+
+window = sg.Window('Km to Miles Converter',
+                   layout=[[label, input_box], [miles_button, output]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    # Using if, elif, and else for event handling
+    if event == "Convert":
+        km = float(values["kms"])
+        result = km_to_miles(km)
+        window['output'].update(value=result)
+    elif event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+
